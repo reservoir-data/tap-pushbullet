ac4548721da25e39a18dda7a3259739e9544aa7b
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-02-25 17:46:17 -0600

    feat: Make `start_date` setting of date-time kind (#409)
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- tap_pushbullet/tap.py
+++ tap_pushbullet/tap.py
@@ -2,10 +2,15 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, override
+
 from singer_sdk import Stream, Tap
 from singer_sdk import typing as th
 
 from tap_pushbullet import streams
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
 
 
 class TapPushbullet(Tap):
@@ -18,21 +23,19 @@ class TapPushbullet(Tap):
             "api_key",
             th.StringType,
             required=True,
+            secret=True,
             description="API Key for Pushbullet",
         ),
         th.Property(
             "start_date",
-            th.NumberType,
-            description="Earliest Unix timestamp to get data from",
+            th.DateTimeType,
+            description="Earliest datetime to get data from",
         ),
     ).to_dict()
 
-    def discover_streams(self) -> list[Stream]:
-        """Return a list of discovered streams.
-
-        Returns:
-            A list of Pushbullet streams.
-        """
+    @override
+    def discover_streams(self) -> Sequence[Stream]:
+        """Return a list of discovered streams."""
         return [
             streams.Chats(tap=self),
             streams.Devices(tap=self),
